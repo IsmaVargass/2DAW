66bef4b085d59c68c0f512deea0efae41d45160d
Ismael <ismaelvargasduque14@gmail.com>
2026-01-24 14:11:19 +0100

    Batería de ejercicios rápidos

---
+++ taskhub/resources/js/Pages/Test.jsx
@@ -0,0 +1,9 @@
+export default function Test() {
+    return (
+        <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+            <h1 className="text-4xl font-bold text-indigo-600">
+                Página de prueba
+            </h1>
+        </div>
+    );
+}
--- taskhub/resources/js/Pages/Welcome.jsx
+++ taskhub/resources/js/Pages/Welcome.jsx
@@ -1,24 +1,29 @@
-import { Link } from '@inertiajs/react';
+import { Link } from "@inertiajs/react";
 
-export default function Welcome() {
+export default function Welcome({ mensaje }) {
     return (
         <div className="min-h-screen bg-slate-100 flex flex-col items-center justify-center">
+            <p className="text-green-600 font-semibold text-lg mb-4">
+                {mensaje}
+            </p>
+
             <h1 className="text-4xl font-black text-indigo-600 uppercase tracking-widest">
                 TaskHub De Ismael Vargas
             </h1>
+
             <p className="text-slate-500 mt-2 font-medium">
                 Aprendiendo Laravel + React para DAW
             </p>
+
             <div className="mt-6 flex gap-4">
-                <Link 
+                <Link
                     href="/tareas"
                     className="bg-indigo-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-indigo-600 transition"
                 >
                     Ver Listado
                 </Link>
-                
-                {/* 👇 NUEVO BOTÓN para el contador */}
-                <Link 
+
+                <Link
                     href="/contador"
                     className="bg-green-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-green-600 transition"
                 >
